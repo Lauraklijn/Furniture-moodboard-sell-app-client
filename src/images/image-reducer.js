15831ac9c31b099c21d36ec18a5bcdcb48c09000
Lauraklijn <lauraklijn12@hotmail.com>
2020-03-09 11:12:49 +0100

    Created Signup component

--- src/images/image-reducer.js
+++ src/images/image-reducer.js
@@ -1,12 +1,12 @@
-import { IMAGES_FETCHED } from "../images/image-action";
+// import { IMAGES_FETCHED } from "../images/image-action";
 
-const ImageReducer = (images = [], action) => {
-  switch (action.type) {
-    case IMAGES_FETCHED:
-      return [...action.images];
-    default:
-      return [...images];
-  }
-};
+// const ImageReducer = (images = [], action) => {
+//   switch (action.type) {
+//     case IMAGES_FETCHED:
+//       return [...action.images];
+//     default:
+//       return [...images];
+//   }
+// };
 
-export default ImageReducer;
+// export default ImageReducer;
